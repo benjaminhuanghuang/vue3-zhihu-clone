f4ec28d6bfe69007abc76f8c376d51d4cef08fff
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2021-04-23 16:05:02 -0700

    columnlist

--- zheye/src/App.vue
+++ zheye/src/App.vue
@@ -1,27 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from "vue";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
+import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
+
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: "Test",
+    description: "description.....",
+    avatar: "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e3a17c305b1070f455202.jpg?x-oss-process=image/resize,m_pad,h_50,w_50"
+  },
+  {
+    id: 2,
+    title: "Test2",
+    description: "description.....",
+    avatar: "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e3a17c305b1070f455202.jpg?x-oss-process=image/resize,m_pad,h_50,w_50"
   }
-})
+];
+export default defineComponent({
+  name: "App",
+  components: {
+    ColumnList
+  },
+  setup () {
+    return {
+      list: testData
+    };
+  }
+});
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
